b90569a0792d373d34ed1332cffe431a9e8e22da
Larry Gadallah <lgadallah@gmail.com>
2015-11-09 22:48:06 -0800

    More tinkering

--- test/correlate.py
+++ test/correlate.py
@@ -3,18 +3,19 @@ from scipy import signal
 import matplotlib.pyplot as plt
 
 # tone synthesis
-def note(freq, len, amp=1, rate=44100):
+def note(freq, len, amp=1, rate=8000):
  t = np.linspace(0,len,len*rate)
  data = np.sin(2*np.pi*freq*t)*amp
  # return data.astype(int16) # two byte integers
  return data
 
-sig = np.repeat([0., 1., 1., 0., 1., 0., 0., 1.], 128)
-#sig = note(357, 0.01)
-sig_noise = sig + np.random.randn(len(sig))
-corr = signal.correlate(sig_noise, np.ones(128), mode='full') / 128
+#sig = np.repeat([0., 1., 1., 0., 1., 0., 0., 1.], 128)
+sig = np.concatenate( (np.zeros(200), note(1000, 0.05) ) )
+sig = np.concatenate( (sig, np.zeros(50) ) )
+sig_noise = 0.20 * sig + 0.80 * np.random.randn(len(sig))
+corr = signal.correlate(sig_noise, sig, mode='same')
 
-clock = np.arange(64, len(sig), 128)
+clock = np.arange(50, len(sig), 100)
 fig, (ax_orig, ax_noise, ax_corr) = plt.subplots(3, 1, sharex=True)
 
 ax_orig.plot(sig)
@@ -26,8 +27,8 @@ ax_noise.set_title('Signal with noise')
 
 ax_corr.plot(corr)
 ax_corr.plot(clock, corr[clock], 'ro')
-ax_corr.axhline(0.1, ls=':')
-ax_corr.set_title('Cross-correlated with rectangular pulse')
+ax_corr.axhline(25.0, ls=':')
+ax_corr.set_title('Cross-correlated with original')
 
 ax_orig.margins(0, 0.1)
 
